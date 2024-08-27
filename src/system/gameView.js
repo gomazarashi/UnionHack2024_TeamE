55ae2f27858f75b5181d395080f53f679dc8b03c
t-a1024 <133000386+t-a1024@users.noreply.github.com>
2024-08-28 02:16:18 +0900

    画面遷移ができるように

--- src/system/gameView.js
+++ src/system/gameView.js
@@ -1,25 +1,42 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+class gameView{
+    constructor(canvas,ctx){
+        this.ctx = ctx;
 
-const player = new playerControl()
+        this.player = new playerControl()
+        this.canvasStyle = new CanvasStyle(canvas,ctx);
+        this.flag = false;
 
-const canvasStyle = new CanvasStyle(canvas);
+        //this.enemy = new Enemy(10,10);
+    }
 
-function update(){
-    ctx.clearRect(0,0,640,480);
+    update(){
+        this.ctx.clearRect(0,0,640,480);
 
-    canvasStyle.drawBackground(); // 背景の描画
-    canvasStyle.drawGameScreen(0, 3); // スコアとライフの表示
+        this.canvasStyle.drawBackground(); // 背景の描画
+        this.canvasStyle.drawGameScreen(0, 3); // スコアとライフの表示
 
-    // アイテムメニューの表示
-    const items = ['Item 1', 'Item 2', 'Item 3'];
-    canvasStyle.drawItemMenu(items);
+        // アイテムメニューの表示
+        const items = ['Item 1', 'Item 2', 'Item 3'];
+        this.canvasStyle.drawItemMenu(items);
 
-    player.movePlayer();
-    player.drawPlayer(ctx);
-    player.bulletControl(ctx);
+        this.player.movePlayer();
+        this.player.drawPlayer(this.ctx);
+        this.player.bulletControl(this.ctx);
 
-    requestAnimationFrame(update);
+        // this.enemy.moveEnemy();
+        // this.enemy.drawEnemy(this.ctx);
+
+        if (this.flag) {
+            requestAnimationFrame(()=>this.update());
+        }
+    }
+
+    gameStart(){
+        this.flag = true;
+        this.update();
+    }
+
+    gameStop(){
+        this.flag = false;
+    }
 }
-
-update();
